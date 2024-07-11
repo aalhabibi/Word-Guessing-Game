ec82e1b30821e74a74dec4725059b18b48da1403
aalhabibi <116291494+aalhabibi@users.noreply.github.com>
2024-07-11 18:40:16 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -115,22 +115,30 @@ let wordIndex = Math.floor(Math.random() * 50);
 let word = words[wordIndex];
 let hint = hints[wordIndex];
 
-document.getElementById("hint").innerHTML = "Hint: " + hint;
+let wrongLetters = [];
+
+document.getElementById("hint").innerHTML =
+  '<span style="font-weight: bold;">Hint: </span>' + hint;
 document.getElementById("remaining-chances").innerHTML =
-  "Remaining Chances: " + chances;
+ '<span style="font-weight: bold;">Remaining Chances</span><br>' + chances;
 
 function reset() {
   for (let index = 0; index < 4; index++) {
     document.getElementById("letter" + (index + 1)).innerHTML = "";
-    document.getElementById("letter" + (index + 1)).style.color = "green";
+    document.getElementById("letter" + (index + 1)).style.color = "#00ff1a";
   }
-
+  document.getElementById("game-status").style.color = "white";
   document.getElementById("guess-textbox").value = "";
 
   document.getElementById("game-status").innerHTML = "Guess The Word!!";
-  document.getElementById("hint").innerHTML = "Hint: " + hint;
+
+  document.getElementById("hint").innerHTML =
+    '<span style="font-weight: bold;">Hint: </span>' + hint;
   document.getElementById("remaining-chances").innerHTML =
-    "Remaining Chances: " + chances;
+'<span style="font-weight: bold;">Remaining Chances</span><br>' + chances;
+
+  document.getElementById("wrong-letters").innerHTML = "";
+  wrongLetters = [];
 
   gameOver = false;
 }
@@ -140,12 +148,15 @@ function win() {
   gameOver = true;
   document.getElementById("game-status").innerHTML =
     "Congrats! You have guessed the word correctly.";
+
+  document.getElementById("game-status").style.color = "#00ff1a";
 }
 
 function lose() {
   gameOver = true;
   document.getElementById("game-status").innerHTML =
     "You ran out of chances :(";
+  document.getElementById("game-status").style.color = "red";
   for (let i = 0; i < word.length; i++) {
     if (!document.getElementById("letter" + (i + 1)).innerHTML) {
       document.getElementById("letter" + (i + 1)).innerHTML = word[i];
@@ -156,16 +167,38 @@ function lose() {
 
 function checkGuess(word) {
   let guess = document.getElementById("guess-textbox").value;
-  console.log(guess);
-  chances--;
 
+  //Checking if input is correct
+
+  if (guess.length != 1 && guess.length != 4) {
+    const gameStatus = document.getElementById("game-status");
+
+    gameStatus.innerHTML =
+      "Please enter a single letter or a full 4-letter word.";
+
+    gameStatus.classList.add("shake");
+
+    gameStatus.addEventListener(
+      "animationend",
+      () => {
+        gameStatus.classList.remove("shake");
+      },
+      { once: true }
+    );
+    return;
+  }
+  // console.log(guess);
+
+  chances--;
   //Comparing every letter of the user's guess with letters of the word
 
   for (let i = 0; i < guess.length; i++) {
+    let found = false;
+
     for (let j = 0; j < word.length; j++) {
       if (guess[i].toUpperCase() === word[j].toUpperCase()) {
         //Check if letter already guessed
-
+        found = true;
         if (
           !(document.getElementById("letter" + (j + 1)).innerHTML == word[j])
         ) {
@@ -174,10 +207,17 @@ function checkGuess(word) {
         }
       }
     }
+    if (!found && !wrongLetters.includes(guess[i].toUpperCase())) {
+      wrongLetters.push(guess[i].toUpperCase());
+    }
+
+    //Guessed 1 letter only and its correct
+
+    if (found && guess.length === 1) chances++;
   }
 
-  console.log("Guessed letters " + guessedLetters);
-  console.log("chances " + chances);
+  // console.log("Guessed letters " + guessedLetters);
+  // console.log("chances " + chances);
   document.getElementById("guess-textbox").value = "";
 
   if (guessedLetters === 4) {
@@ -186,7 +226,18 @@ function checkGuess(word) {
     lose();
   }
   document.getElementById("remaining-chances").innerHTML =
-    "Remaining Chances: " + chances;
+    '<span style="font-weight: bold;">Remaining Chances</span><br>' + chances;
+
+  if (wrongLetters.length != 0) {
+    let wrongLettersString = "";
+    for (let index = 0; index < wrongLetters.length; index++) {
+      wrongLettersString += wrongLetters[index] + " ";
+    }
+    document.getElementById("wrong-letters").innerHTML =
+      '<span style="font-weight: bold;">Wrong Guessed Letters</span><br>' +
+      wrongLettersString;
+    wrongLettersString = "";
+  }
 }
 
 function loadGame() {
@@ -211,13 +262,25 @@ function loadGame() {
     console.log(word);
   });
 
-  //Guess Button Listener
+  //Guess button listener
 
   guessButton.addEventListener("click", function () {
     if (!gameOver) {
       checkGuess(word);
     }
   });
+
+  //Enter key listener
+
+  document
+    .getElementById("guess-textbox")
+    .addEventListener("keydown", function (event) {
+      if (event.key === "Enter") {
+        if (!gameOver) {
+          checkGuess(word);
+        }
+      }
+    });
 }
 
 loadGame();
